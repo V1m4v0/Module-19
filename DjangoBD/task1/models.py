1b3c2ed97a413b92854286ef1b3a2d21c417b085
V1m4v0 <almax.crazy2015@yandex.ru>
2024-12-05 11:18:44 +0300

    Add files via upload

--- DjangoBD/task1/models.py
+++ DjangoBD/task1/models.py
@@ -14,7 +14,7 @@ class Game(models.Model):
     size = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='username')
+    buyer = models.ManyToManyField(Buyer)
 
     def __str__(self):
         return self.title
