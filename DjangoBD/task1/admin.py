6f60b2c7895c5f3b7d92dd514ab296369f5d0254
V1m4v0 <almax.crazy2015@yandex.ru>
2024-12-10 08:38:49 +0300

    Add files via upload

--- DjangoBD/task1/admin.py
+++ DjangoBD/task1/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_field = ['title']
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_field = ['name']
+    list_per_page = 30
+    readonly_field = ['balance']
+
